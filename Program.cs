4979d2702fda12c23893eb32fb4629a0478aa3fc
timurka12 <93543993+timurka12@users.noreply.github.com>
2022-03-23 23:02:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+enum Types { Накопляющий, Текущий };
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Bank bank = new Bank();
+        Console.WriteLine("Введите номер аккаунта: ");
+        bank.n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите тип аккаунта: ");
+        bank.t = Console.ReadLine();
+        Console.WriteLine("Введите количество денег на счету: ");
+        bank.q = int.Parse(Console.ReadLine());
+        Console.WriteLine("Тип банковского аккаунта " + bank.t + ", номер счета " + bank.n + ", количество " + bank.q);
+        Console.ReadKey();
+    }
+}
+public class Bank
+{
+    private int number, count;
+    private string type;
+    public string t { get { return type; } set { type = value; } }
+    public int n { get { return number; } set { number = value; } }
+    public int q { get { return count; } set { count = value; } }
+    public void Rt(Bank bank, int a)
+    {
+        bank.q -= a;
+        this.q += a;
+    }
+}

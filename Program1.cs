ed977ea59d3e1faac17964f19c3fe9598ed75e2b
timurka12 <93543993+timurka12@users.noreply.github.com>
2022-04-04 16:02:26 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+enum Types{ Накопляющий, Текущий };
+    public class Program
+{
+    public static void Main(string[] args)
+    {
+        Bank bank = new Bank();
+        Console.WriteLine("Введите номер аккаунта: ");
+        bank.number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите тип аккаунта: ");
+        bank.type = Console.ReadLine();
+        Console.WriteLine("Введите количество денег на счету: ");
+        bank.quantity = int.Parse(Console.ReadLine());
+        Console.WriteLine("Тип банковского аккаунта " + bank.type + ", номер счета " + bank.number + ", количество " + bank.quantity);
+        Console.ReadKey();
+    }
+}
+public class Bank
+{
+    private int amount , count;
+    private string style;
+    public string type { get { return style; } set { style = value; } }
+    public int number { get { return amount; } set { amount = value; } }
+    public int quantity { get { return count; } set { count = value; } }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Bank bank &&
+               amount == bank.amount &&
+               count == bank.count &&
+               style == bank.style &&
+               type == bank.type &&
+               number == bank.number &&
+               quantity == bank.quantity;
+    }
+}
